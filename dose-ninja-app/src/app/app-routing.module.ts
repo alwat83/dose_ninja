e19bbfb90c40f41156c7281560be90c2d04e553d
alwat83 <albertlwatson@gmail.com>
2024-05-12 03:36:25 +0000

    update to app module

--- dose-ninja-app/src/app/app-routing.module.ts
+++ dose-ninja-app/src/app/app-routing.module.ts
@@ -1,6 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { routes } from './app.routes';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from 'src/app/register/register.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- dose-ninja-app/src/app/app/app.module.ts
+++ dose-ninja-app/src/app/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { MedicationManagementComponent } from './medication-management/medication-management.component';
+import { AppComponent } from '../app.component'; 
+import { LoginComponent } from '../login/login.component'; 
+import { MedicationManagementComponent } from '../medication-management/medication-management.component'; 
+import { AppRoutingModule } from '../app.routes'; 
 import { MedicationService } from './services/medication.service';
-
 @NgModule({
-  declarations: [AppComponent, MedicationManagementComponent],
-  imports: [BrowserModule],
-  providers: [MedicationService], // Register the MedicationService as a provider
+  declarations: [AppComponent, LoginComponent, MedicationManagementComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [MedicationService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
-providers: [MedicationService]
