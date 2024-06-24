0a7b5c6e3d02faf8c6f20399d4e2a3e2fed708e5
alwat83 <albertlwatson@gmail.com>
2024-06-24 13:35:26 +0000

    update

--- dose-ninja-app/src/app/app.module.ts
+++ dose-ninja-app/src/app/app.module.ts
@@ -1,12 +1,19 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
-
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MedicationManagementComponent } from './medication-management/medication-management.component';
+import { AppRoutingModule } from './app.routes';
+import { MedicationService } from './app/services/medication.service';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [AppComponent, LoginComponent, MedicationManagementComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [MedicationService],
+  schemas: const [CUSTOM_ELEMENTS_SCHEMA],
+  bootstrap: [AppComponent],
+  exports: [MedicationManagementComponent],
 })
-export class AppModule { }
+export class AppModule {}
+export class MedicationManagementModule {}
